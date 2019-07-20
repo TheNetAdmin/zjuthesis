ba5200d31e566571258eb20885623e085383d4e3
TheNetAdmin <zxwang42@gmail.com>
2019-07-20 03:16:18 -0700

    Prepare new ci script.

--- script/ci/build_all.sh
+++ script/ci/build_all.sh
@@ -3,16 +3,18 @@
 texfile=zjuthesis.tex
 
 function build() {
-    # $1 major
-    # $2 type
-    # $3 period
-    # $4 blind
-    sed -i -r "s/MajorFormat\s*=\s*.*/MajorFormat = $1,/" $texfile
-    sed -i -r "s/Type\s*=\s*.*/Type = $2,/" $texfile
-    sed -i -r "s/Period\s*=\s*.*/Period = $3,/" $texfile
-    sed -i -r "s/BlindReview\s*=\s*.*/BlindReview = $4,/" $texfile
+    # $1 degree
+    # $2 major
+    # $3 type
+    # $4 period
+    # $5 blind
+    sed -i -r "s/Degree\s*=\s*.*/Degree = $1,/" $texfile
+    sed -i -r "s/MajorFormat\s*=\s*.*/MajorFormat = $2,/" $texfile
+    sed -i -r "s/Type\s*=\s*.*/Type = $3,/" $texfile
+    sed -i -r "s/Period\s*=\s*.*/Period = $4,/" $texfile
+    sed -i -r "s/BlindReview\s*=\s*.*/BlindReview = $5,/" $texfile
 
-    echo -e "Build major[$1]\ttype[$2]\tperiod[$3]\tblind[$4]"
+    echo -e "Build degree[$1]\tmajor[$2]\ttype[$3]\tperiod[$4]\tblind[$5]"
     latexmk -xelatex -outdir=out
 
     blind_suffix=""
@@ -21,13 +23,17 @@ function build() {
         blind_suffix="-blind"
     fi
 
-    mv out/zjuthesis.pdf dist/zjuthesis-$1-$2-$3$blind_suffix.pdf
+    mv out/zjuthesis.pdf dist/zjuthesis-$1-$2-$3-$4$blind_suffix.pdf
 }
 
 project_dir="$( cd "$(dirname "$0")" ; pwd -P )"/../../
 echo "CD $project_dir"
 cd $project_dir
 
+
+# Build undergraduate
+
+degree="undergraduate"
 main_majors=("general" "cs")
 sample_majors=("isee" "math" "physics")
 types=("design" "thesis")
@@ -35,16 +41,28 @@ periods=("final" "proposal")
 
 mkdir -p dist
 
-build "general" "thesis" "final" "true"
+build $degree "general" "thesis" "final" "true"
 
 for major in ${main_majors[@]}; do
     for period in ${periods[@]}; do
         for type in ${types[@]}; do
-            build $major $type $period "false"
+            build $degree $major $type $period "false"
         done
     done
 done
 
 for major in ${sample_majors[@]}; do
-    build $major "thesis" "final" "false"
+    build $degree $major "thesis" "final" "false"
+done
+
+
+# Build graduate
+
+degree="graduate"
+types=("design" "thesis" "equal")
+
+build $degree "general" "thesis" "final" "true"
+
+for type in ${types[@]}; do
+    build $degree "general" $type "final" "false"
 done
