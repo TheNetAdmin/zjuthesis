14fed58de686b31f19b415384cec704fcbe11f9f
TheNetAdmin <wzxlovesy@gmail.com>
2019-12-11 15:17:37 +0800

    Update ci script.

--- TODO.md
+++ TODO.md
@@ -3,7 +3,7 @@
 - [ ] CS master template
   - [x] Change linespread in pages/graduate/cs/cover-eng.tex
   - [ ] Check margin space for general graduate template
-- [ ] English template
+- [x] English template
 - [ ] Describe graduate template dir structure in README
 - [x] Use title macro in cover
   - Well, not quite feasible, as there are whitespaces in title strings
--- script/ci/build_all.sh
+++ script/ci/build_all.sh
@@ -8,13 +8,17 @@ function build() {
     # $3 type
     # $4 period
     # $5 blind
+    # $6 grad_level
+    # $7 language
     sed -i -r "s/Degree\s*=\s*.*/Degree = $1,/" $texfile
     sed -i -r "s/MajorFormat\s*=\s*.*/MajorFormat = $2,/" $texfile
     sed -i -r "s/Type\s*=\s*.*/Type = $3,/" $texfile
     sed -i -r "s/Period\s*=\s*.*/Period = $4,/" $texfile
     sed -i -r "s/BlindReview\s*=\s*.*/BlindReview = $5,/" $texfile
+    sed -i -r "s/GradLevel\s*=\s*.*/GradLevel = $6,/" $texfile
+    sed -i -r "s/Language\s*=\s*.*/Language = $7,/" $texfile
 
-    echo -e "Build degree[$1]\tmajor[$2]\ttype[$3]\tperiod[$4]\tblind[$5]"
+    echo -e "Build degree[$1]\tmajor[$2]\ttype[$3]\tperiod[$4]\tblind[$5]\tgrad_level[%6]\tlanguage[$7"
     latexmk -xelatex -outdir=out
 
     blind_suffix=""
@@ -23,7 +27,19 @@ function build() {
         blind_suffix="-blind"
     fi
 
-    mv out/zjuthesis.pdf dist/zjuthesis-$1-$2-$3-$4$blind_suffix.pdf
+    grad_suffix=""
+    if [ $1 == "graduate" ]
+    then
+        grad_suffix="-master"
+    fi
+
+    lang_suffix=""
+    if [ $7 == "english" ]
+    then
+        lang_suffix="-english"
+    fi
+
+    mv out/zjuthesis.pdf dist/zjuthesis-$1-$2$grad_suffix-$3-$4$blind_suffix$lang_siffix.pdf
 }
 
 project_dir="$( cd "$(dirname "$0")" ; pwd -P )"/../../
@@ -46,13 +62,13 @@ build $degree "general" "thesis" "final" "true"
 for major in ${main_majors[@]}; do
     for period in ${periods[@]}; do
         for type in ${types[@]}; do
-            build $degree $major $type $period "false"
+            build $degree $major $type $period "false" "doctor" "chinese"
         done
     done
 done
 
 for major in ${sample_majors[@]}; do
-    build $degree $major "thesis" "final" "false"
+    build $degree $major "thesis" "final" "false" "doctor" "chinese"
 done
 
 
@@ -61,8 +77,11 @@ done
 degree="graduate"
 types=("design" "thesis" "equal")
 
-build $degree "general" "thesis" "final" "true"
+build $degree "general" "thesis" "final" "true" "doctor" "chinese"
 
 for type in ${types[@]}; do
-    build $degree "general" $type "final" "false"
+    build $degree "general" $type "final" "false" "doctor" "chinese"
 done
+
+build "graduate" "cs"      "thesis" "final" "false" "master" "chinese"
+build "graduate" "general" "thesis" "final" "false" "doctor" "english"
