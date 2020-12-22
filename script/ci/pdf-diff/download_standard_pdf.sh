1ceda4228ca3f66a6bbcc9e9622e84e6353e3fda
TheNetAdmin <zxwang42@gmail.com>
2020-12-22 14:37:35 -0800

    Add script to download pdfs from GitHub release

---
+++ script/ci/pdf-diff/download_standard_pdf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
+out_dir=${1:-out/standard}
+repo=${2:-thenetadmin/zjuthesis}
+tag=${3:-v6.4.0}
+standard_url=https://api.github.com/repos/${repo}/releases/tags/${tag}
+
+mkdir -p $out_dir
+pushd $out_dir
+curl -s $standard_url | grep url | grep pdf | cut -d '"' -f 4 | wget -qi -
+popd
