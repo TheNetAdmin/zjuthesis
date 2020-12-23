14be98db36c042969609010085d20ea8136c9cce
TheNetAdmin <zxwang42@gmail.com>
2020-12-22 16:19:26 -0800

    Add pdf diff scripts

---
+++ script/ci/pdf-diff/diff_report.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Downloading standard baseline pdf files"
+bash ./script/ci/pdf-diff/download_standard_pdf.sh
+
+echo "Start comparing and generating pdf diff report"
+mkdir -p diff
+
+pushd diff
+
+rm -f report.txt
+touch report.txt
+# PDF diff report
+# 0: two pdf files visually match
+# 1: two pdf files has slight visually mismatch
+# 2: two pdf files has significant visually mismatch
+# 3: standard baseline pdf is missing
+
+for fname in $(ls ../dist); do
+    fbase=${fname%.*}
+    echo Checking $fbase
+    echo -n "$fbase: " >>report.txt
+    mkdir -p $fbase
+    cp ../dist/$fname         $fbase/cur-$fname
+    if [[ -f ../out/standard/$fname ]]; then
+        cp ../out/standard/$fname $fbase/std-$fname
+        pushd $fbase
+        python -m diff_pdf_visually -v cur-$fname std-$fname >$fbase.log
+        echo $? >>../report.txt
+        popd
+    else
+        echo "3" >>report.txt
+    fi
+done
+
+popd
+
+result=$(cat diff/report.txt | cut -d ' ' -f 2 | paste -sd+ - | bc)
+exit $result
