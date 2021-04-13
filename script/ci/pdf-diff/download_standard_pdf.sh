6f160d8e8a589f185fdb040b2321f658268bcc11
TheNetAdmin <zxwang42@gmail.com>
2021-04-13 01:38:19 -0700

    Update pdf-diff script

--- script/ci/pdf-diff/download_standard_pdf.sh
+++ script/ci/pdf-diff/download_standard_pdf.sh
@@ -2,11 +2,9 @@
 
 # https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
 out_dir=${1:-out/standard}
-repo=${2:-thenetadmin/zjuthesis}
-tag=${3:-standard}
-standard_url=https://api.github.com/repos/${repo}/releases/tags/${tag}
 
-mkdir -p $out_dir
-pushd $out_dir
-curl -s $standard_url | grep url | grep pdf | cut -d '"' -f 4 | wget -i -
-popd
+mkdir -p "$out_dir"
+pushd "$out_dir" || exit 1
+git clone --depth=1 https://github.com/TheNetAdmin/zjuthesis-std.git
+cp zjuthesis-std/baseline/*.pdf .
+popd || exit 1
